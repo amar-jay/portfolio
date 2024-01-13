f9f931479df52e420e77dcedbc3968c2dad42b7d
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-01-13 19:48:21 +0300

    themes, and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,12 +52,83 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      backgroundImage: {
+              "gradient-radial":
+                      "radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 3s ease-in-out forwards",
+        "title": "title 3s ease-out forwards",
+        "fade-left": "fade-left 3s ease-in-out forwards",
+        "fade-right": "fade-right 3s ease-in-out forwards",
+      },
       keyframes: {
+        "fade-in": {
+              "0%": {
+                      opacity: "0%",
+              },
+              "75%": {
+                      opacity: "0%",
+              },
+              "100%": {
+                      opacity: "100%",
+              },
+      },
+      "fade-left": {
+              "0%": {
+                      transform: "translateX(100%)",
+                      opacity: "0%",
+              },
+
+              "30%": {
+                      transform: "translateX(0%)",
+                      opacity: "100%",
+              },
+              "100%": {
+                      opacity: "0%",
+              },
+      },
+      "fade-right": {
+              "0%": {
+                      transform: "translateX(-100%)",
+                      opacity: "0%",
+              },
+
+              "30%": {
+                      transform: "translateX(0%)",
+                      opacity: "100%",
+              },
+              "100%": {
+                      opacity: "0%",
+              },
+      },
+      title: {
+              "0%": {
+                      "line-height": "0%",
+                      "letter-spacing": "0.25em",
+                      opacity: "0",
+              },
+              "25%": {
+                      "line-height": "0%",
+                      opacity: "0%",
+              },
+              "80%": {
+                      opacity: "100%",
+              },
+
+              "100%": {
+                      "line-height": "100%",
+                      opacity: "100%",
+              },
+      },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -67,11 +138,9 @@ module.exports = {
           to: { height: "0" },
         },
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
 }
